bc4a1537aec53eb5a382ed0ff282b4f2ae2ebb79
chrisndoo <39110398+chrisndoo@users.noreply.github.com>
2021-07-08 18:19:15 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+from app import app
+import ny
+
+
+@app.route('/')
+
+def index():
+  
+
+
+
+    return '''
+<html>
+    <head> 
+    ''' + x0 + '''
+        
+    </head>
+    <body>
+        <h1>Hello!</h1>
+        <a href="https://www.w3schools.com">here</a>
+        ''' + x + '''
+    </body>
+</html>'''
+
