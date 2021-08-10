8fa97995ef82b247301d6fc816d1c16779af1108
chrisndoo <39110398+chrisndoo@users.noreply.github.com>
2021-08-10 15:06:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import ny
 app=Flask(__name__)
 
 @app.route('/')
@@ -7,23 +7,34 @@ app=Flask(__name__)
 def index():
    
 
+    headdict = ny.get_headlines()
+
     x0 = '<title>' + '头条' + '</title>'
 
+    i=0
+    x =''
+    for i in headdict:
+    
+        #x1 = '<p>' + headdict[i][0] + '</p>'
+        
+        x2 = '<a href="' + headdict[i][2] + '">' + '<p>' + headdict[i][1] + '</p>' + '</a>' 
+
+        x = x+x2
 
 
-
-    return '''
+   return '''
 <html>
     <head> 
     ''' + x0 + '''
         
     </head>
     <body>
-        <h1>Hello4!</h1>
+        <h1>Hello!</h1>
         <a href="https://www.w3schools.com">here</a>
-   
+        ''' + x + '''
     </body>
 </html>'''
+
   
 
 
